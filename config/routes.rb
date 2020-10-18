1793d6d0c2c2b78e961f259e6efc83d22b222f8c
Yasunari_Kainuna <yasu6491@icloud.com>
2020-10-18 19:08:25 +0900

    routes.rb edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-
   root 'static_pages#top'
-#  root :to => 'users#index'
-
   get  'static_pages/top'
-    
   resources :users
-  get  'login'  => 'user_sessions#new',     :as => :login
+  get  'login'  => 'user_sessions#new', :as => :login
   post 'login'  => 'user_sessions#create'
   post 'logout' => 'user_sessions#destroy', :as => :logout
-
 end
