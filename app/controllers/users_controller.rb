2b08123968832cb2808dea10ee1f2cfe523ce238
Yasunari_Kainuna <yasu6491@icloud.com>
2020-10-18 19:03:05 +0900

    user_controller edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[:show, :edit, :update, :destroy]
-  skip_before_action :require_login, only: %i[:index, :new, :create]
+  before_action :set_user, only: %i[show edit update destroy]
+  skip_before_action :require_login, only: %i[index new create]
   # GET /users
   # GET /users.json
   def index
@@ -24,17 +24,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-   # respond_to do |format|
-      if @user.save
-        redirect_to(login_path, notice: 'User was successfully created')
-         # format.html { redirect_to @user, notice: 'User was successfully created.' }
-         # format.json { render :show, status: :created, location: @user }
-      else
-        render :new
-         # format.html { render :new }
-         # format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-   # end
+  # respond_to do |format|
+    if @user.save
+      redirect_to(login_path, notice: 'User was successfully created')
+      # format.html { redirect_to @user, notice: 'User was successfully created.' }
+      # format.json { render :show, status: :created, location: @user }
+    else
+      render :new
+      # format.html { render :new }
+      # format.json { render json: @user.errors, status: :unprocessable_entity }
+    end
+  # end
   end
 
   # PATCH/PUT /users/1
@@ -62,13 +62,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
+  end
 end
