bc189b3b7e548d7e517d4ab0e9fca27919543dda
Yasunari_Kainuna <yasu6491@icloud.com>
2020-10-18 18:16:00 +0900

    app_controller edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   private
-
   def not_authenticated
     redirect_to login_path, alert: 'Please login first'
   end
