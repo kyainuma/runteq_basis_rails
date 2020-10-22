9115a602df725160418e09a44a191ba4095c905d
Yasunari_Kainuna <yasu6491@icloud.com>
2020-10-22 12:02:40 +0900

    finish task7

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,7 +15,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    #redirect_to root_path, success: t('.success')
     redirect_to root_path, success: t('.success')
   end
 end
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,15 +1,6 @@
 class UserDecorator < Draper::Decorator
   delegate_all
   def full_name
-    last_name + " " + first_name
+    last_name + ' ' + first_name
   end
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
 end
