d780aad39392ca59513ceaf6e92a256fdd22b984
Yasunari_Kainuna <yasu6491@icloud.com>
2020-10-22 21:31:19 +0900

    Boardsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_122318) do
+ActiveRecord::Schema.define(version: 2020_10_22_122222) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -18,8 +27,8 @@ ActiveRecord::Schema.define(version: 2020_10_17_122318) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "last_name"
-    t.string "first_name"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
