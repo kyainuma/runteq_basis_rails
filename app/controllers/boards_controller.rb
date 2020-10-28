872e2e5c53f08f0c6c3776d24afac4c392054ba0
Yasunari_Kainuna <yasu6491@icloud.com>
2020-10-28 18:33:09 +0900

    アップローダークラスの作成・紐付け　board_imageカラム追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -22,6 +22,6 @@ class BoardsController < ApplicationController
   private
 
   def board_params
-    params.require(:board).permit(:title, :body)
+    params.require(:board).permit(:title, :body, :board_image)
   end
 end
