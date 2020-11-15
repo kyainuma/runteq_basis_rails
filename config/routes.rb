73ece44d778962f904542e5eaa02c25eebd9f63b
Yasunari_Kainuna <yasu6491@icloud.com>
2020-11-15 21:38:55 +0900

    task15 routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   resources :boards do
     resources :comments, only: %i[create update destroy], shallow: true
     resource  :bookmarks, only: %i[create destroy]
-    get 'bookmarks', on: :collection
+    collection do
+      get :bookmarks
+    end
   end
+  resources :bookmarks, only: %i[create destroy]
 end
