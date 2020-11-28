744497af924f860a06a57846be685df6fa178783
Yasunari_Kainuna <yasu6491@icloud.com>
2020-11-28 17:01:03 +0900

    管理画面 掲示板一覧実装

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -1,2 +1,18 @@
 class Admin::BoardsController < ApplicationController
+
+  def index
+    @q = Board.ransack(params[:q])
+    @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
+  end
+
+  def set_board
+    @board = current_user.boards.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
     resources :users, only: [:index, :show, :edit, :update, :destroy]
+    resources :boards, only: [:index, :show, :edit, :update, :destroy]
   end
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
