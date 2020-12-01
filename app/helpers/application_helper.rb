e833f2a52f66218466c6f02960c096ad6fb6d1e0
Yasunari_Kainuna <yasu6491@icloud.com>
2020-12-02 06:25:33 +0900

    掲示板メニュー　アクティブ化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,23 @@ module ApplicationHelper
                  end
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def active_controller?(controller_name)
+    params[:controller] == controller_name ? 'active' : nil
+  end
+  def active_controllers?(controller_names)
+    controller_names.include?(params[:controller]) ? 'active' : nil
+  end
+  def active_action?(action_name)
+    params[:action] == action_name ? 'active' : nil
+  end
+  def active_actions?(action_names)
+    action_names.include?(params[:action]) ? 'active' : nil
+  end
+  def active_controller_and_action?(controller_name, action_name)
+    if params[:controller] == controller_name && params[:action] == action_name
+      return 'active'
+    end
+    nil
+  end
 end
