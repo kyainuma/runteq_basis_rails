c2c22b50358dcc2821c76ce62764bbe933e599ea
Yasunari_Kainuna <yasu6491@icloud.com>
2020-12-04 21:26:48 +0900

    例外処理変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   unless Rails.env.development?
+  # rescue_from ActionController::RoutingError,   with: :render_404
     rescue_from ActiveRecord::RecordNotFound, with: :error_404
-    rescue_from Exception, with: :error_500
+  # rescue_from Exception, with: :error_500
   end
 
   def error_404
-    render file: Rails.root.join('public/404.html'),
+    render file: Rails.root.join('public', '404'),
            status: 404, layout: false
   end
 
   def error_500(error)
     logger.error error
     logger.error error.backtrace.join("\n\n")
-    render file: Rails.root.join('public/500.html'),
+    render file: Rails.root.join('public', '500'),
            status: 500, layout: false
   end
 
